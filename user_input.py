2104bd73add6c6feb083f17ffc364a9741a215ec
mayabi67 <mathiasmayabi@students.uonbi.ac.ke>
2024-03-05 22:20:39 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Prompt the user for their name
+name = input("What is your name? ")
+
+# Prompt the user for their age
+age = input("How old are you? ")
+
+# Prompt the user for their location
+location = input("Where are you located? ")
+
+# Print out a personalized message
+#print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+print('Hello '+name+' you are '+age+' years old and live in '+location+'.')
